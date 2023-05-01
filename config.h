6a6a6a040d1d4949224c0ddf7a5ae24e7396a2d7
KidCharlemagne4 <coughwil000@gmail.com>
2023-04-30 23:35:38 -0500

    wip alpha patch

--- config.h
+++ config.h
@@ -2,21 +2,28 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xa0;
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 1000;                    /* minimum width when centered */
+static int min_width = 2000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+};  
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha }, 
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 12;
+static unsigned int lines      = 20;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -25,4 +32,4 @@ static unsigned int lines      = 12;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 5;
